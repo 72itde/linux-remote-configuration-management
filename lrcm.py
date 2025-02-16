2f3a92d8a73a96dbe939ca82be6a44e7a675522e
Florian Gusinde <florian.gusinde@googlemail.com>
2025-02-16 20:09:19 +0100

    remove senseless comments

--- lrcm.py
+++ lrcm.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
-# lrcm.py - linux remote configuration management agent
-#
+# lrcm.py - linux remote configuration management
 # Github: https://github.com/72itde/linux-remote-configuration-management
-# Developer: https://www.72it.de/#tab-contact
-
-#
-# imports
-#
+# Developer: Flo
+# contact: https://www.72it.de/#tab-contact
 
 from optparse import OptionParser
 import os
@@ -31,8 +27,6 @@ import random
 from time import sleep
 import logging.handlers
 
-# We get it from distro.name(pretty=True)
-
 LINUX_DISTRIBUTIONS = [
     'Fedora Linux 39 (Workstation Edition)',
     'Debian GNU/Linux 12 (bookworm)',
@@ -51,10 +45,7 @@ PYTHON_VERSIONS = [
 DISTRIBUTION_PRETTY = str(distro.name(pretty=True))
 PLATFORM_PYTHON_VERSION = str(platform.python_version())
 
-# enable garbage collection
 gc.enable
-
-# functions
 
 def remove_pidfile_and_quit(PIDFILE):
     # remove pidfile
@@ -63,8 +54,6 @@ def remove_pidfile_and_quit(PIDFILE):
     del PIDFILE
     exit(0)
     return true
-
-# parse options
 
 parser = OptionParser()
 parser.add_option("-c", "--configfile", dest="configfile", default="/etc/lrcm/lrcm.conf", help="custom config file")
@@ -77,12 +66,6 @@ parser.add_option("-j", "--cronjobs", dest="cronjobs", default=True, help="manag
 if str(options.debug) == "True":
   logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-# ----------------------------------------------------------------------
-# config check
-# ----------------------------------------------------------------------
-
-# check if configfile is readable
-
 if os.access(options.configfile, os.R_OK):
 	logging.info("configfile "+options.configfile+" is readable")
 else:
@@ -94,16 +77,12 @@ if str(options.cronjobs) == "True":
 else:
     logging.info("cronjobs management disabled")
 
-# memory logging
 def log_memory_usage():
-    # On linux, defaults to KB
     memory_usage_kb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     memory_usage_mb = memory_usage_kb / 1024
     logging.debug("memory usage: {:.3f}MB".format(memory_usage_mb))
 
 log_memory_usage()
-
-# read configfile
 
 config = configparser.ConfigParser()
 config.read(options.configfile)
@@ -138,39 +117,16 @@ DAILY_CRONJOB = config.get('CRONJOB', 'daily_cronjob', fallback='False')
 
 PIDFILE = config.get('PIDFILE', 'pidfile', fallback='/run/lrcm.pid')
 
-# get hostname
-
 myhostname = str(os.uname().nodename)
 logging.info("myhostname: "+str(myhostname))
-
-# data type and rule check for config values
-
-# DELAY_BEFORE_START_SECONDS unsigned integer
 
 if (not DELAY_BEFORE_START_SECONDS >= 0):
     logging.error("wrong value for delay_before_start_seconds")
     exit(1)
 
-# DELAY_BEFORE_START_RANDOM_MAX_SECONDS unsigned integer
-
 if (not DELAY_BEFORE_START_RANDOM_MAX_SECONDS >= 0):
     logging.error("wrong value for delay_before_start_random_max_seconds")
     exit(1)
-
-
-# REPOSITORY url
-# BRANCH string
-# PLAYBOOK filename
-# AUTHENTICATION_REQUIRED boolean
-# USERNAME string
-# TOKEN string
-# REBOOT_CRONJOB boolean
-# HOURLY_CRONJOB boolean
-# DAILY_CRONJOB boolean
-# PIDFILE filename
-
-# check platform support
-
 
 if (DISTRIBUTION_PRETTY in LINUX_DISTRIBUTIONS):
     logging.info("Distribution "+DISTRIBUTION_PRETTY+" is supported")
@@ -183,32 +139,20 @@ if (PLATFORM_PYTHON_VERSION in PYTHON_VERSIONS):
 else:
     logging.error("Python version "+PLATFORM_PYTHON_VERSION+" is not supported")
     exit(1)
-
-# ----------------------------------------------------------------------
-# pidfile management
-# ----------------------------------------------------------------------
-
-# check if pidfile already exists
-
 if (os.path.isfile(PIDFILE)):
     logging.warning("pidfile "+PIDFILE+" exists")
-    # if exists, compare content to own pid
     mypid = os.getpid()
     logging.info("process id: "+str(mypid))
     pidfile = open(PIDFILE, 'r')
-    # Read the first line of the file
     pidinpidfile = pidfile.readline()
     logging.info("pid found in pidfile: "+str(pidinpidfile).strip())
     pidfile.close()
-    # ToDo: check if pidinpidfile is an unsigned integer
-    # check if process is running for pidinpidfile
     log_memory_usage()
     if (psutil.pid_exists(int(pidinpidfile))):
         logging.info("process with pid "+str(pidinpidfile).strip()+" is running. Bye.")
         exit(0)
     else:
         logging.warning("process with pid "+str(pidinpidfile).strip()+" is not running")
-        # write my pid to pidfile
         pidfile = open(PIDFILE, 'w')
         pidfile.write(str(mypid))
         pidfile.close()
@@ -216,7 +160,6 @@ if (os.path.isfile(PIDFILE)):
         log_memory_usage()
 else:
     logging.info("pidfile "+PIDFILE+" does not exist")
-	# check if pidfile path is writable
     if (os.access(os.path.dirname(PIDFILE), os.W_OK)):
         logging.info("pidfile path "+str(os.path.dirname(PIDFILE))+" is writable")
         mypid = os.getpid()
@@ -232,34 +175,22 @@ else:
         logging.error("pidfile path "+str(os.path.dirname(PIDFILE))+" is not writable")
         exit(1)
 
-
 logging.debug("DELAY_BEFORE_START_SECONDS: "+str(DELAY_BEFORE_START_SECONDS))
 logging.debug("DELAY_BEFORE_START_RANDOM_MAX_SECONDS: "+str(DELAY_BEFORE_START_RANDOM_MAX_SECONDS))
 mysleeptime = (DELAY_BEFORE_START_SECONDS+random.randrange(0, DELAY_BEFORE_START_RANDOM_MAX_SECONDS))
 logging.debug("mysleeptime: "+str(mysleeptime))
 sleep(mysleeptime)
 
-
-
-# create temporary directory
-
 workdir = tempfile.mkdtemp()
 logging.info("workdir: "+str(workdir))
 
-# clone repository into workdir
-
 git.Repo.clone_from(REPOSITORY_FULL_URL, workdir, branch=BRANCH, depth=1)
-
-# check if playbook file exists
 
 if (os.access(os.path.dirname(workdir+"/"+PLAYBOOK), os.R_OK)):
     logging.info("playbook "+str(workdir+"/"+PLAYBOOK)+" found")
 else:
     logging.error("playbook "+str(workdir+"/"+PLAYBOOK)+" not found")
     remove_pidfile_and_quit(PIDFILE)
-
-
-# run playbook
 
 def ansible_runner_event_handler(event):
     if (dump := event.get("stdout")):
@@ -282,33 +213,21 @@ def run_ansible_runner(PLAYBOOK):
 
 run_ansible_runner(PLAYBOOK)
 
-
-
-# check if a playbook for this host exists
-# debug: copy main playbook to host playbook
-# shutil.copyfile(workdir+"/"+PLAYBOOK, workdir+"/"+myhostname+"-"+PLAYBOOK)
-
 if (os.path.isfile(workdir+"/"+myhostname+"-"+PLAYBOOK)):
     logging.info("host-specific playbook found: "+str(workdir+"/"+myhostname+"-"+PLAYBOOK))
-    # host-specific playbook run
     run_ansible_runner(PLAYBOOK+"-"+myhostname)
 else:
     logging.info("no host-specific playbook found: "+str(workdir+"/"+myhostname+"-"+PLAYBOOK))
 
 def manage_cronjob(special_time, state):
     log_memory_usage()
-    # manage cronjobs
     logging.info("manage cronjobs")
-    # determine absolute path of this file
     logging.info("script path: "+os.path.abspath(__file__))
-    # add some variables
     CRONJOB_SPECIAL_TIME = special_time
     CRONJOB_JOB = os.path.abspath(__file__)+" --configfile="+os.path.abspath(options.configfile)
-    # template a playbook file for cronjobs
     File = open(os.path.abspath(os.path.dirname(__file__))+'/templates/cronjob.yaml.j2', 'r')
     content = File.read() 
     File.close() 
-    # Render the template and pass the variables 
     template = Template(content) 
     rendered = template.render(CRONJOB_SPECIAL_TIME=CRONJOB_SPECIAL_TIME, CRONJOB_JOB=CRONJOB_JOB, CRONJOB_STATE=CRONJOB_STATE, CRONJOB_FILE_STATE=CRONJOB_FILE_STATE)
     generated_cronjob_playbook_filename = workdir+"/"+"generated_playbook_"+special_time+".yaml"
@@ -327,7 +246,6 @@ def manage_cronjob(special_time, state):
     gc.collect()
     log_memory_usage()
     return True
-
 
 if str(options.cronjobs) == "True":
     if (str(config.get('CRONJOB', 'hourly_cronjob')) == "True" ):
@@ -351,9 +269,6 @@ if str(options.cronjobs) == "True":
         CRONJOB_STATE = "absent"
         CRONJOB_FILE_STATE = "absent"
     manage_cronjob("daily", DAILY_CRONJOB)
-
-
-# remove workdir recursively
 
 shutil.rmtree(workdir)
 logging.info("workdir deleted")
