4feb5421562d01656248a662d08e2d91342da0a4
Florian Gusinde <florian.gusinde@googlemail.com>
2025-02-24 12:07:02 +0100

    remove the --unmask-token parameter option

--- lrcm.py
+++ lrcm.py
@@ -58,7 +58,6 @@ def remove_pidfile_and_quit(PIDFILE):
 parser = OptionParser()
 parser.add_option("-c", "--configfile", dest="configfile", default="/etc/lrcm/lrcm.conf", help="custom config file")
 parser.add_option("-d", "--debug", action="store_true", dest="debug", default=False, help="run in debug mode")
-parser.add_option("-u", "--unmask-token", action="store_true", dest="unmasktoken", default=False, help="unmask token in debug mode")
 parser.add_option("-j", "--cronjobs", dest="cronjobs", default=True, help="manage cronjobs")
 
 (options, args) = parser.parse_args()
