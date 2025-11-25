d9265104a64bb9f53464327be7dc806c06b31dd2
Florian Gusinde <florian.gusinde@72it.de>
2025-11-25 18:39:04 +0100

    add Fedora Linux 43 (COSMIC) and Python 3.14.0

--- lrcm.py
+++ lrcm.py
@@ -36,14 +36,16 @@ LINUX_DISTRIBUTIONS = [
     'elementary OS 8',
     'Ubuntu 24.04.2 LTS',
     'Ubuntu 24.04.3 LTS',
-    'openSUSE Tumbleweed'
+    'openSUSE Tumbleweed',
+    'Fedora Linux 43 (COSMIC)'
     ]
 PYTHON_VERSIONS = [
     '3.10.12',
     '3.11.2',
     '3.12.0',
     '3.12.3',
-    '3.13.9'
+    '3.13.9',
+    '3.14.0'
 ]
 
 DISTRIBUTION_PRETTY = str(distro.name(pretty=True))
